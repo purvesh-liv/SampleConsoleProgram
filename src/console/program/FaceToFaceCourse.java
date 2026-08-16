0a278e26be93f91abf24ad2e4b5e41103c51577d
purvesh-liv <ppurvesh60@gmail.com>
2026-08-15 20:24:23 -0700

    implemented facetofacecourse class

--- src/console/program/FaceToFaceCourse.java
+++ src/console/program/FaceToFaceCourse.java
@@ -1,5 +1,22 @@
 package console.program;
 
 public class FaceToFaceCourse extends Course {
-    
+
+    // private extra feilds 
+    private int capacity;
+    private int currentEnrollments;
+
+    // constructor
+    public FaceToFaceCourse(String courseName, String year, String mode, String dayOfLecture, int startTime,
+            int duration,int capacity,int currentEnrollments) {
+        super(courseName, year, mode, dayOfLecture, startTime, duration);
+        this.capacity = capacity;
+        this.currentEnrollments = currentEnrollments;
+
+    }
+    // overding parent method
+    @Override
+    public boolean hasRoom(){
+     return currentEnrollments < capacity;
+}
 }
